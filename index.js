04d16193d5ac04c7eeba17400914b723dc203a7b
zyrouge <naveencnaveen96@gmail.com>
2020-04-11 13:56:02 +0530

    Version 2.0.0 :0

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-module.exports = {
-  Client: require("./lib/client")
-};
+'use strict';
+
+const Client = require("./lib/Client");
+
+function Genius(token, options) {
+    return new Client(token, options);
+}
+
+Genius.Client = Client;
+Genius.version = require("./package.json").version;
+Genius.Tracks = require("./lib/util/Tracks");
+Genius.Track = require("./lib/util/Track");
+
+module.exports = Genius;
