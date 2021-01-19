15143e75917e61d25f1ab765813a6124c3bbe82b
Vitalii Kravets <vitalii.kravets@gmail.com>
2021-01-19 22:33:40 +0100

    Fix config check.

--- tests/simple.ts
+++ tests/simple.ts
@@ -1,8 +1,10 @@
-import Genius from "../dist";
-import util from "util";
+import Genius, { Constants } from "../dist";
+import * as util from "util";
 
 const Token = "GjZdsQku4iZ7k9QRw0v3g2PRBRe6zrAe3NzPLBqdA-fBEFC_u1DbxuFF5FZvSxPL";
 const Client = new Genius.Client(Token);
+const Config = {origin: {api: Constants.BASE_URL, url: 'https://genius.com'}};
+const ClientWithConfig = new Genius.Client(Token, Config);
 const wait = util.promisify(setTimeout);
 
 const test = async () => {
@@ -28,6 +30,10 @@ const test = async () => {
     const againArtist = await Client.artists.get(againArtistNo);
     console.log(`\nAbout Artist ${againArtistNo}:`);
     console.log(againArtist);
+
+    const searchesWithConfig = await ClientWithConfig.songs.search("faded");
+    console.log("Number of songs found with custom config:");
+    console.log(searchesWithConfig.length);
 
     console.log(`\nCompleted in ${Date.now() - startTime}ms`);
 };
