0be5ba229862179725914c6609fecd3d458e8eaa
ZYROUGE <naveencnaveen96@gmail.com>
2021-03-08 21:25:19 +0530

    ✨ Fixed default user-agent

--- lib/Constants.ts
+++ lib/Constants.ts
@@ -12,7 +12,7 @@ export const Constants = {
     BASE_URL: "https://api.genius.com",
     UN_BASE_URL: "https://genius.com/api",
     ARTIST_SORTS: ["title", "popularity"],
-    DEF_USER_AGENT: "Mozilla/5.0 (Windows NT x.y; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0",
+    DEF_USER_AGENT: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36",
     REQUIRES_KEY: "This action requires a valid Genius Token",
     INV_RES_OBJ: "Invalid response object",
     INV_CONFIG_OBJ: "Invalid config",
